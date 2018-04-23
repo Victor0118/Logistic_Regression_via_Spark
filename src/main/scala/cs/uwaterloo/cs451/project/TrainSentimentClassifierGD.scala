d3b3fc1c9aff878dc8dbc64481b2a5a7ce59f14b
qiumasijia <qiumasijia@gmail.com>
2018-04-23 00:00:38 -0400

    update gd

--- src/main/scala/cs/uwaterloo/cs451/project/TrainSentimentClassifierGD.scala
+++ src/main/scala/cs/uwaterloo/cs451/project/TrainSentimentClassifierGD.scala
@@ -3,6 +3,7 @@ package ca.uwaterloo.cs451.project
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.log4j.Logger
 import org.apache.spark.{SparkConf, SparkContext}
+import org.rogach.scallop._
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -11,7 +12,7 @@ import scala.collection.mutable.ArrayBuffer
   */
 
 
-object TrainSentimentClassifierGD {
+object TrainSentimentClassifierMBSGD {
   val log = Logger.getLogger(getClass().getName())
 
   def main(argv: Array[String]) {
@@ -20,11 +21,11 @@ object TrainSentimentClassifierGD {
     log.info("Model: " + args.model())
     log.info("Shuffle: " + args.shuffle())
     log.info("epoch: " + args.epoch().toString())
-    log.info("fraction: " + args.fraction().toString())
     log.info("regularization: " + args.regularization().toString())
+    log.info("fraction: " + args.fraction().toString())
     log.info("lr: " + args.lr().toString())
 
-    val conf = new SparkConf().setAppName("TrainerGD")
+    val conf = new SparkConf().setAppName("TrainerMBSGD")
     val sc = new SparkContext(conf)
     val textFile = sc.textFile(args.input())
     val outputDir = new Path(args.model())
@@ -40,36 +41,7 @@ object TrainSentimentClassifierGD {
       (0, (docid, pos, features, rand))
     })
 
-    val feature_counter = scala.collection.mutable.Map[Int, Double]()
-    val feature_count = inputFeature.mapPartitions(partition => {
-      val buffer = ArrayBuffer[scala.collection.mutable.Map[Int, Double]]()
-      val f_counter = scala.collection.mutable.Map[Int, Double]()
-      partition.foreach( pair => {
-        val instance = pair._2
-        val features = instance._3
-        features.foreach(f=> {
-          if (f_counter.contains(f)) {
-            f_counter(f) += 1
-          } else {
-            f_counter(f) = 1
-          }
-        })
-      }
-      )
-      buffer.+=(f_counter)
-      buffer.iterator
-    }).collect().foreach( collection => {
-      collection.keys.foreach(f=>{
-        if (feature_counter.contains(f)) {
-          feature_counter(f) += collection(f)
-        } else {
-          feature_counter(f) = collection(f)
-        }
-      })
-    }
-
-    )
-
+    val batch_size = (inputFeature.count() * args.fraction()).toInt * 1.0
 
     if (args.shuffle()) {
       inputFeature = inputFeature.sortBy(pair => pair._2._4)
@@ -77,21 +49,24 @@ object TrainSentimentClassifierGD {
 
     var w_total = scala.collection.mutable.Map[Int, Double]()
     val reg = args.regularization()
-    val delta = 0.002
+    val delta = args.lr()
 
     for (iter <- 1 to args.epoch()) {
       val w = sc.broadcast(w_total)
 
-      val gradient = inputFeature.mapPartitions(partition => {
-        val buffer = ArrayBuffer[scala.collection.mutable.Map[Int, Double]]()
-        val g = scala.collection.mutable.Map[Int, Double]()
+      val temp_feature = scala.collection.mutable.Map[Int, Double]()
+      val temp_counter = scala.collection.mutable.Map[Int, Double]()
 
+      val gradient = inputFeature.mapPartitions(partition => {
+
+        val buffer = ArrayBuffer[(scala.collection.mutable.Map[Int, Double], scala.collection.mutable.Map[Int, Double])]()
+        val g = scala.collection.mutable.Map[Int, Double]()
+        val counter = scala.collection.mutable.Map[Int, Double]()
         def sentiment(features: Array[Int]): Double = {
           var score = 0d
           features.foreach(f => if (w.value.contains(f)) score += w.value(f))
           score
         }
-
         partition.foreach(pair => {
           val instance = pair._2
           val docid = instance._1
@@ -102,24 +77,40 @@ object TrainSentimentClassifierGD {
           features.foreach(f => {
             if (g.contains(f)) {
               g(f) += (pos - prob - 2.0 * w.value.getOrElse(f, 0.0) * reg) * delta
+              counter(f) += 1
             } else {
               g(f) = (pos - prob - 2.0 * w.value.getOrElse(f, 0.0) * reg) * delta
+              counter(f) = 1
             }
           })
 
         })
-        buffer.+=(g)
+        buffer.+=((g, counter))
         buffer.iterator
       }
-      ).collect().foreach(g => {
+      ).collect().foreach(pair => {
+        val g = pair._1
+        val c = pair._2
         g.keys.foreach(f => {
-          if (w_total.contains(f)) {
-            w_total(f) += g(f) / feature_counter(f)
+          if (temp_feature.contains(f)) {
+            temp_feature(f) += g(f)
+            temp_counter(f) += c(f)
           } else {
-            w_total(f) = g(f) / feature_counter(f)
+            temp_feature(f) = g(f)
+            temp_counter(f) = c(f)
           }
         })
       }
+      )
+
+      temp_feature.keys.foreach( f=> {
+        if (w_total.contains(f)) {
+          w_total(f) += temp_feature(f) / temp_counter(f)
+        } else {
+          w_total(f) = temp_feature(f) / temp_counter(f)
+        }
+      }
+
       )
     }
     sc.parallelize(w_total.toSeq).coalesce(1).saveAsTextFile(args.model())
